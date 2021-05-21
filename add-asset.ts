ef050e589256bed96aaf4d7fc4bc2a76a8064cc4
Mohamed Ismail <ismailbuharimohamed@gmail.com>
2021-05-21 19:56:55 +0530

    Create add-asset.ts

---
+++ add-asset.ts
@@ -0,0 +1,8 @@
+export class AddAsset {
+    assetName: string;
+    assetCategory: string;
+    dateOfPurchase: Date;
+    assetCost: number;
+    assetDescription: string;
+    assetId: string;
+}
