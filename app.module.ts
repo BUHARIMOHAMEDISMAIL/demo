7a3ace865a589f475dab3e5fa2ac9adfc3eea8cc
Mohamed Ismail <ismailbuharimohamed@gmail.com>
2021-05-21 19:52:59 +0530

    Create app.module.ts

---
+++ app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AddAssetComponent } from './add-asset/add-asset.component';
+import { ViewAssetComponent } from './view-asset/view-asset.component';
+import { UpdateAssetComponent } from './update-asset/update-asset.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddAssetComponent,
+    ViewAssetComponent,
+    UpdateAssetComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
